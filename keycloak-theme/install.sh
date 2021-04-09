9619f7d03cb0c2fdaf440cd7e88dc03cc7952a92
alxlenc <1325588+alxlenc@users.noreply.github.com>
2021-04-09 19:33:03 +0200

    add keycloak custom music theme and installation script

---
+++ keycloak-theme/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+install_dir=../src/main/docker/keycloak
+jar_filename=music.jar
+theme_jar=target/$jar_filename
+source_folder=themes
+
+[[ ! -d "$source_folder/theme" ]] && echo "Themes must be based on a 'theme' folder" && exit 1
+
+[[ ! -d target ]] && mkdir "target"
+jar cfvM $theme_jar -C $source_folder .
+
+# Expand relative path for logging purposes
+echo "Installing to dir $(
+  cd "$(dirname "$install_dir")"
+  pwd
+)/$(basename "$install_dir")"
+
+rm $install_dir/deploy/*
+cp $theme_jar $install_dir/deploy/$jar_filename
+
+echo "Theme $jar_filename installed successfully!!!"
