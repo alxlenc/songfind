adad897a56915a608734e0f2aa6d10a8250d8a0e
alxlenc <1325588+alxlenc@users.noreply.github.com>
2021-04-11 20:37:04 +0200

    Revert "attempt to copy docker-compose.yml file to containerized docker-compose client"
    This reverts commit f78e578b51e104cba5f9013a9b268dc2ada83427.

--- src/test/java/alx/music/songfind/SongfindApplicationIT.java
+++ src/test/java/alx/music/songfind/SongfindApplicationIT.java
@@ -3,31 +3,19 @@ package alx.music.songfind;
 import java.io.File;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testcontainers.containers.ContainerState;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.utility.MountableFile;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-//@Testcontainers
+@Testcontainers
 class SongfindApplicationIT {
 
   @Container
-  private final static DockerComposeContainer keycloak;
-
-
-  public static final String DOCKER_COMPOSE_YML = "./src/main/docker/keycloak/docker-compose.yml";
-
-  static {
-    keycloak = new DockerComposeContainer(
-        new File(DOCKER_COMPOSE_YML))
-        .withExposedService("keycloak", 9080, Wait.forHttp("/auth").forPort(9080).forStatusCode(200))
-        .withLocalCompose(false);
-    keycloak.start();
-    ContainerState  cs = (ContainerState) keycloak.getContainerByServiceName("keycloak_1").get();
-    cs.copyFileToContainer(MountableFile.forHostPath(DOCKER_COMPOSE_YML, 0744),  DOCKER_COMPOSE_YML);
-  }
+  private final static DockerComposeContainer keycloak = new DockerComposeContainer(
+      new File("src/main/docker/keycloak/docker-compose.yml"))
+      .withExposedService("keycloak", 9080, Wait.forHttp("/auth").forPort(9080).forStatusCode(200));
 
   @Test
   void contextLoads() {
