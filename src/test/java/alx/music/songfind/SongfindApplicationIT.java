6ccdf07828f6d0221b2458568dc68c4d062eea5d
alxlenc <1325588+alxlenc@users.noreply.github.com>
2021-04-12 16:24:32 +0200

    set integration test with testcontainer GenericContainer and adapted configuration from the compose file

--- src/test/java/alx/music/songfind/SongfindApplicationIT.java
+++ src/test/java/alx/music/songfind/SongfindApplicationIT.java
@@ -2,20 +2,58 @@ package alx.music.songfind;
 
 import java.io.File;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testcontainers.containers.DockerComposeContainer;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @Testcontainers
 class SongfindApplicationIT {
 
+  private static Slf4jLogConsumer logConsumer =
+      new Slf4jLogConsumer(LoggerFactory.getLogger(SongfindApplicationIT.class));
+
+  private final static String KEYCLOACK_BASE_PATH  = new File("src/main/docker/keycloak").getAbsolutePath();
+
   @Container
-  private final static DockerComposeContainer keycloak = new DockerComposeContainer(
-      new File("src/main/docker/keycloak/docker-compose.yml"))
-      .withExposedService("keycloak", 9080, Wait.forHttp("/auth").forPort(9080).forStatusCode(200));
+  private final static GenericContainer<?> keycloak =
+      new GenericContainer<>(DockerImageName.parse("jboss/keycloak:12.0.4"))
+          .withEnv("KEYCLOAK_USER", "admin")
+          .withEnv("KEYCLOAK_PASSWORD", "admin")
+          .withEnv("DB_VENDOR", "h2")
+          .withCommand(
+              "-b",
+              "0.0.0.0",
+              "-Dkeycloak.migration.action=import",
+              "-Dkeycloak.migration.provider=dir",
+              "-Dkeycloak.migration.dir=/opt/jboss/keycloak/realm-config",
+              "-Dkeycloak.migration.strategy=OVERWRITE_EXISTING",
+              "-Djboss.socket.binding.port-offset=1000",
+              "-Dkeycloak.profile.feature.upload_scripts=enabled")
+      .withExposedPorts(9080, 9443, 10990)
+      .withLogConsumer(logConsumer)
+      .withFileSystemBind(KEYCLOACK_BASE_PATH + "/realm-config", "/opt/jboss/keycloak/realm-config")
+      .withFileSystemBind(KEYCLOACK_BASE_PATH + "/deploy", "/opt/jboss/keycloak/standalone/deployments")
+      ;
+
+  static {
+    keycloak.setWaitStrategy(Wait.forHttp("/auth").forPort(9080).forStatusCode(200));
+  }
+
+  @DynamicPropertySource
+  static void authServer(DynamicPropertyRegistry registry) {
+    Integer mappedPort = keycloak.getMappedPort(9080);
+    registry.add("application.auth.server", () -> "localhost:" + mappedPort);
+    registry.add("spring.security.oauth2.client.provider.songfind.issuer-uri",
+        () -> "http://localhost:" + mappedPort + "/auth/realms/music");
+  }
 
   @Test
   void contextLoads() {
