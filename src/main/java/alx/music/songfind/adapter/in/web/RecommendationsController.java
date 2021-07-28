97d3072d57921a60d32487ae7a54ed7738d30b0d
alxlenc <1325588+alxlenc@users.noreply.github.com>
2021-07-28 14:34:34 +0200

    add log line to recommendations endpoint for testing (#10)

--- src/main/java/alx/music/songfind/adapter/in/web/RecommendationsController.java
+++ src/main/java/alx/music/songfind/adapter/in/web/RecommendationsController.java
@@ -6,6 +6,7 @@ import alx.music.songfind.application.port.in.GetRecommendationsCommand;
 import alx.music.songfind.application.port.in.GetRecommendationsQuery;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/recommendations")
 @RequiredArgsConstructor
+@Slf4j
 class RecommendationsController {
 
   private final GetRecommendationsQuery getRecommendationsQuery;
@@ -22,6 +24,9 @@ class RecommendationsController {
   @GetMapping
   public Recommendations getRecommendations(
       @RequestParam(value = "seed_artists", required = false) List<String> artistIds) {
+
+    log.info("Requesting recommendations for artists: {}", artistIds);
+
     GetRecommendationsCommand getRecommendationsCommand = new GetRecommendationsCommand(artistIds);
     alx.music.songfind.domain.Recommendations recommendations = this.getRecommendationsQuery
         .getRecommendations(getRecommendationsCommand);
