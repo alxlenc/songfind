986acae4a7f80000422f2ce35ada9e153df9df88
alxlenc <1325588+alxlenc@users.noreply.github.com>
2021-08-28 17:47:24 +0200

    increment test coverage

--- src/test/java/alx/music/songfind/adapter/in/web/SpotifyAccountControllerTest.java
+++ src/test/java/alx/music/songfind/adapter/in/web/SpotifyAccountControllerTest.java
@@ -1,17 +1,24 @@
 package alx.music.songfind.adapter.in.web;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import alx.music.songfind.adapter.in.web.mapper.SpotifyUserMapper;
+import alx.music.songfind.adapter.in.web.mapper.SpotifyUserMapperImpl;
 import alx.music.songfind.application.port.in.GetSpotifyUserQuery;
 import alx.music.songfind.config.TestSecurityConfiguration;
+import alx.music.songfind.config.WithLoggedUser;
+import alx.music.songfind.domain.Followers;
+import alx.music.songfind.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+import reactor.core.publisher.Mono;
 
 
 @WebMvcTest(SpotifyAccountController.class)
@@ -26,8 +33,8 @@ public class SpotifyAccountControllerTest {
   @MockBean
   private GetSpotifyUserQuery getSpotifyUserQuery;
 
-  @MockBean
-  private SpotifyUserMapper spotifyUserMapper;
+  @SpyBean
+  private SpotifyUserMapperImpl spotifyUserMapper;
 
   @Test
   public void unauthorizedRequestReturnsUnauthorized() throws Exception {
@@ -39,6 +46,34 @@ public class SpotifyAccountControllerTest {
   public void loginEndpointReturnsRedirect() throws Exception {
     this.mockMvc.perform(get("/login/oauth2/authorization/spotify"))
         .andExpect(status().is3xxRedirection());
+  }
+
+  @Test
+  @WithLoggedUser
+  public void getSpotifyAccountReturnsOk() throws Exception {
+    // Arrange
+    User spotifyUser = this.getUser();
+    when(this.getSpotifyUserQuery.getCurrentAccount()).thenReturn(Mono.just(spotifyUser));
+
+    // Act
+    this.mockMvc.perform(get("/api/spotify/account")).andExpect(status().isOk())
+        .andExpect(jsonPath("$.id").value("userId"))
+        .andExpect(jsonPath("$.email").value("user@user.com"))
+        .andExpect(jsonPath("$.name").value("UserName"))
+        .andExpect(jsonPath("$.country").value("ES"))
+        .andExpect(jsonPath("$.id").value("userId"))
+        .andExpect(jsonPath("$.followers.href").value("followers_ref"))
+        .andExpect(jsonPath("$.followers.total").value("20"))
+    ;
+
+  }
+
+  private User getUser() {
+    User spotifyUser = new User("userId", "UserName", "user@user.com");
+    Followers followers = new Followers("followers_ref", 20);
+    spotifyUser.setFollowers(followers);
+    spotifyUser.setCountry("ES");
+    return spotifyUser;
   }
 
 }
