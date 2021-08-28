d0eea6b819e77ab052c8916fd2f511775233354d
alxlenc <1325588+alxlenc@users.noreply.github.com>
2021-08-28 16:08:24 +0200

    add SpotifyClient missing tests

--- src/test/java/alx/music/songfind/adapter/out/web/spotify/SpotifyClientTest.java
+++ src/test/java/alx/music/songfind/adapter/out/web/spotify/SpotifyClientTest.java
@@ -19,15 +19,71 @@ import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @ExtendWith(MockitoExtension.class)
 public class SpotifyClientTest {
 
   private SpotifyClient sut;
 
+  @Test
+  void getCurrentAccount() {
+    // Arrange
+    String path = "/v1/me";
+
+    // Assert
+    this.sut = this.supplySpotifyClientWithRequestAssertions(
+        clientRequest -> assertThat(clientRequest.url().getPath()).isEqualTo(path));
+
+    // Act
+    StepVerifier.create(this.sut.getCurrentAccount()).expectSubscription().verifyComplete();
+
+  }
 
   @Test
-  void getRecommendationsSetsRequestParamsOk() {
+  void getCurrentUserPlaylistsRequestIsWellFormed() {
+    // Arrange
+    int offset = 20;
+    String path = "/v1/me/playlists";
+
+    // Assert
+    this.sut = this.supplySpotifyClientWithRequestAssertions(clientRequest -> {
+      assertThat(clientRequest.url().getPath()).isEqualTo(path);
+      Map<String, String> actualParams = this.extractQueryParams(clientRequest);
+      assertThat(actualParams.get("offset")).isEqualTo(Integer.toString(offset));
+    });
+
+    // Act
+    StepVerifier.create(this.sut.getCurrentUserPlaylists(offset))
+        .expectSubscription()
+        .verifyComplete();
+  }
+
+  @Test
+  void getPlaylistTracksRequestIsWellFormed() {
+
+    // Arrange
+    int offset = 10;
+    String playlistId = "playlist1";
+
+    // Assert
+    this.sut = this.supplySpotifyClientWithRequestAssertions((clientRequest -> {
+
+      assertThat(clientRequest.url().getPath()).contains(playlistId);
+      Map<String, String> actualParams = this.extractQueryParams(clientRequest);
+      assertThat(actualParams.get("offset")).isEqualTo(Integer.toString(offset));
+
+    }));
+
+    // Act
+    StepVerifier.create(this.sut.getPlaylistTracks(offset, playlistId))
+        .expectSubscription()
+        .verifyComplete();
+  }
+
+
+  @Test
+  void getRecommendationsSetsRequestIsWellFormed() {
     // Arrange
     GetRecommendationsQueryParam queryParams = GetRecommendationsQueryParam.builder()
         .artistIds(List.of("1A", "2B"))
@@ -45,7 +101,9 @@ public class SpotifyClientTest {
       assertThat(actualParams.get("target_popularity")).isEqualTo("70");
     });
     // Act
-    this.sut.getRecommendations(queryParams).block();
+    StepVerifier.create(this.sut.getRecommendations(queryParams))
+        .expectSubscription()
+        .verifyComplete();
   }
 
   private Map<String, String> extractQueryParams(ClientRequest clientRequest) {
