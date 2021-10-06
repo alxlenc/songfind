475943116c478ddebde68b980d4ee955c7649d29
alxlenc <1325588+alxlenc@users.noreply.github.com>
2021-10-06 13:04:08 +0200

    clean imports

--- src/main/java/alx/music/songfind/common/UseCase.java
+++ src/main/java/alx/music/songfind/common/UseCase.java
@@ -5,12 +5,12 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Component
+@Service
 public @interface UseCase {
 
 }
--- src/main/java/alx/music/songfind/common/config/SearchArtistConfiguration.java
+++ src/main/java/alx/music/songfind/common/config/SearchArtistConfiguration.java
@@ -1,12 +1,10 @@
 package alx.music.songfind.common.config;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("application.artist")
-@Getter
-@Setter
+@Data
 public class SearchArtistConfiguration {
 
   private Integer maxSearchResults;
