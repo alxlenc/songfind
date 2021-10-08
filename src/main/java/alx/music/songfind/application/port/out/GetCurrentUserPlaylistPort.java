7782c41598496ce442561999cc5f65c7a3ac08d8
alxlenc <1325588+alxlenc@users.noreply.github.com>
2021-10-08 17:34:23 +0200

    clean imports

--- src/main/java/alx/music/songfind/application/port/out/GetCurrentUserPlaylistPort.java
+++ src/main/java/alx/music/songfind/application/port/out/GetCurrentUserPlaylistPort.java
@@ -1,7 +1,6 @@
 package alx.music.songfind.application.port.out;
 
 import alx.music.songfind.domain.Playlist;
-import alx.music.songfind.domain.PlaylistTrack;
 import reactor.core.publisher.Flux;
 
 public interface GetCurrentUserPlaylistPort {
